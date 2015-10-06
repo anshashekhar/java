ede924da652f41ad1a218b490bc4513472b70fa8
Jayant Shekhar <jayantshekhar@Jayants-iMac.local>
2015-10-05 22:10:08 -0700

    added one more house

--- src/House.java
+++ src/House.java
@@ -1,11 +1,23 @@
 
 public class House {
 
+	public String name;
 	public String color = "";
 	public int numBedrooms;
+	public String address;
+	
+	public House(String nm, String clr, int numbed, String addr) {
+		name = nm;
+		color = clr;
+		numBedrooms = numbed;
+		address = addr;
+	}
 	
 	public void print() {
+		System.out.println("Name is "+name);
 		System.out.println("Color is "+color);
 		System.out.println("Num Bedrooms is "+numBedrooms);
+		System.out.println("Address is "+address);
+		System.out.println("--------------");
 	}
 }
--- src/RunHouse.java
+++ src/RunHouse.java
@@ -2,16 +2,25 @@
 public class RunHouse {
 
 	public static void main(String args[]) {
-		House anshahouse = new House();
-		anshahouse.color = "cream";
-		anshahouse.numBedrooms = 3;
 		
-		House suhanihouse = new House();
-		suhanihouse.color = "blue";
-		suhanihouse.numBedrooms = 4;
+		run();
+	}
+	
+	private static void run() {
+		// create the houses
+		House anshahouse = new House("ansha house", "cream", 3, "milpitas, ca");
+		House suhanihouse = new House("suhani house", "blue", 4, "milpitas, ca");
+		House shiprahouse = new House("shipra hose", "red", 6, "fremont, ca");
 		
+		// print the details of the houses
 		anshahouse.print();
 		suhanihouse.print();
+		shiprahouse.print();
+		
+		
+		// find the total number of bedrooms in all the houses
+		int totalbed = anshahouse.numBedrooms + suhanihouse.numBedrooms + shiprahouse.numBedrooms;
+		System.out.println("total number of bedrooms "+totalbed);
 	}
 	
 }
