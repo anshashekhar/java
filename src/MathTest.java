00ba5bfad1b7d9007d1f13cccfb483f96e3dc05c
Jayant Shekhar <jayantshekhar@Jayants-iMac.local>
2016-03-06 15:40:16 -0800

    Math test

---
+++ src/MathTest.java
@@ -0,0 +1,35 @@
+
+public class MathTest
+{
+         public static void main (String[] args)
+         {
+                 int num1 = 10; //setting to 10
+                 int num2 = 3; // setting to 3
+                 // declare 3 int variables: 10,3,0
+                 
+                 //sum = 10 + 3 : 13
+                 int sum = num1 + num2;
+                 System.out.println(num1 + " plus " + num2 + " Equals; " + sum);
+                 
+                 //sum = 10 + 3 :13
+                 sum = num1 + num2;
+                 System.out.println("Adding sum;" + sum);
+                 
+                 //sum = 10 - 3 : 7
+                 sum = num1 - num2;
+                 System.out.println(num1 + " : minus " + num2 + " Equals: " + sum);
+                 
+                 //sum = 10 + 3 ; 13
+                 sum = num1 - num2;
+                 System.out.println( " Minus sum:" + sum);
+                 
+                 //sum = 10 / 3 : 13
+                 sum = num1 - num2;
+                 
+                 //sum = 10 + 3 : 13
+                 sum = num1 - num2;
+                 
+           }
+ }
+                 
+               
--- src/MyFirstJavaProgram.java
+++ src/MyFirstJavaProgram.java
@@ -1,6 +1,6 @@
 
 
-public class JavaProgram {
+public class MyFirstJavaProgram {
 		
 	    public static void main(String []args) {
 	       System.out.println("Hello"); // prints Hello
