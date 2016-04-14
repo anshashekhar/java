f9a87dc4612ec1400649df64a7fe5755542942db
Jayant Shekhar <jayant@Nikhils-MacBook-Air.local>
2016-04-13 18:34:43 -0700

    even numbers

---
+++ src/EvenNumbers.java
@@ -0,0 +1,18 @@
+
+public class EvenNumbers {
+	public static void main(String[] args) {
+        
+      int limit= 100;
+       
+        System.out.println();
+       
+        for (int i=1; i <= limit; i++) {
+              
+        	if( i % 2 == 0)
+        		System.out.print(i +" ");
+            }
+        
+	}
+}
+
+
---
+++ src/FindTotal.java
@@ -0,0 +1,40 @@
+public class FindTotal { 
+	public static void main (String[] args)
+	{
+	int sum = 0;
+	// for loop
+
+	for (int i = 1; i<= 100; i++){
+	    sum = sum + i;
+	}
+	System.out.println(sum);
+
+	// while loop
+
+	sum = 0;
+	int j = 1;
+
+	while(j<=100){
+	    sum = sum + j;
+	    j++;
+	}
+
+	System.out.println(sum);
+
+	// do while loop
+
+	sum = 0;
+	j = 1;
+
+	do{
+	    sum = sum + j;
+	    j++;
+	}
+	while(j<=100);
+
+	System.out.println(sum);
+	}
+}
+	
+
+	
