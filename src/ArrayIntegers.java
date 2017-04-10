e8b5f9474a820320af1199367f2373fd647ea2f3
Jayant Shekhar <jayant@Jayant.local>
2017-04-09 17:26:57 -0700

    new program

---
+++ src/ArrayIntegers.java
@@ -0,0 +1,19 @@
+
+public class ArrayIntegers {
+
+	public static void main(String[] args) {
+		int[] intArray = new int[3]; {
+	
+		intArray[0] = 1;
+		intArray[1] = 2;
+		intArray[2] = 3;
+	
+		for (int i=0; i<intArray.length; i++)
+	
+		  System.out.println(intArray[i]);
+		}
+	}
+}
+
+
+
