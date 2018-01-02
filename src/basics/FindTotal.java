3cd6d0a3763d3eba2f9d3b1056c21ad1c31b4f0c
Jayant Shekhar <jayant@Jayant.local>
2018-01-02 11:55:20 -0800

    moved to basics package

--- src/basics/FindTotal.java
+++ src/basics/FindTotal.java
@@ -1,3 +1,4 @@
+package basics;
 public class FindTotal { 
 	public static void main (String[] args)
 	{
---
+++ src/basics/Prime.java
@@ -0,0 +1,9 @@
+package basics;
+
+public class Prime {
+
+	public static void main(String[] args) {
+		System.out.println("Find prime numbers between 1-100");
+			
+	}
+}
