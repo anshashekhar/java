95287e1162174328f4683de82632c6e28b7a1113
Jayant Shekhar <jayant@Jayant.local>
2018-01-02 18:11:22 -0800

    ap cs practice program 1

--- src/ap/year2017/digits/Digits.java
+++ src/ap/year2017/digits/Digits.java
@@ -1,7 +1,26 @@
 package ap.year2017.digits;
 
+import java.util.ArrayList;
+
 // https://secure-media.collegeboard.org/secure/ap/pdf/ap-computer-science-a-frq-2017.pdf?__gda__=1514923347_d6023384cbfa0f33c19a5e0b2dbc0062
 
-public class Digits {
-
+public class Digits
+{
+		public static void main(String[] args) {
+			
+			int num = 0;
+			
+			ArrayList<Integer> digitList = new ArrayList<Integer>();
+			if (num == 0);
+			{
+				digitList.add(new Integer (0));
+			}
+			while (num > 0);
+			{
+				digitList.add(0, new Integer(num % 10));
+				num /= 10;
+			}
+		}
+	
 }
+
--- src/test/ClassCastTest.java
+++ src/test/ClassCastTest.java
@@ -1,5 +1,2 @@
 package test;
 
-public class ClassCastTest {
-
-}
