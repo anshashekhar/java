707b392729730a1f4abffc1cf72f8459795c90cc
Jayant Shekhar <jayant@Jayant.local>
2018-01-06 16:59:18 -0800

    print results

--- src/ap/barrons/chapter3/IntObject.java
+++ src/ap/barrons/chapter3/IntObject.java
@@ -1,27 +1,47 @@
 package ap.barrons.chapter3;
 
 public class IntObject {
+	
 	private int num;
+	
 	public IntObject()
 	{ num = 0; }
+	
 	public IntObject(int n )
 	{ num = n; }
+	
 	public void increment()
 	{ num++; }
+	
 	public static IntObject someMethod(IntObject obj)
 	{
 		IntObject ans = obj;
 		ans.increment();
 		return ans;
 	}
+	
+	public String toString()
+	{
+		String str = ""+num;
+		return str;
+	}
+	
 	public static void main(String[] args)
 	{
 		IntObject x = new IntObject(2);
+		System.out.println(x.toString());
 		IntObject y = new IntObject(7);
+		System.out.println(y);
+		
 		IntObject a = y;
 		x = someMethod(y);
 		a = someMethod(x);
+		
+		System.out.println("x="+x);
+		System.out.println("y="+y);
+		System.out.println("a="+a);
 	}
+	
 }
 
 
